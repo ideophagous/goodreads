cb0dae10b12e32a42c673cddc7cd1ea98da37e0f
Mounir Afifi <mounir.afifi@intern.promatis.de>
2019-05-02 14:54:02 +0200

    adding load_access function

--- get_gr_page.py
+++ get_gr_page.py
@@ -3,15 +3,26 @@ from   selenium.webdriver.firefox.options import Options
 from   selenium.webdriver.common.keys     import Keys
 from   bs4                                import BeautifulSoup
 
-EMAIL = '############'
-PASSWORD = '#######'
+EMAIL    = '############'
+PASSWORD = '############'
+LINK     = 'https://www.goodreads.com/'
+
+def load_access():
+    with open('access.txt','r') as ac:
+        data       = ac.read()
+        data_parts = data.split('\n')
+        EMAIL      = data_parts[0].strip()
+        PASSWORD   = data_parts[1].strip()
+
+    return EMAIL,PASSWORD
 
 def gr_login(driver):
-    username = driver.find_element_by_id("userSignInFormEmail").send_keys(EMAIL)
-    print(username)
-    password = driver.find_element_by_id("user_password")
+    EMAIL,PASSWORD = load_access()
+    username       = driver.find_element_by_id("userSignInFormEmail").send_keys(EMAIL)
+    #print(username)
+    password       = driver.find_element_by_id("user_password")
     password.send_keys(PASSWORD)
-    print(password)
+    #print(password)
     password.submit()
     #ff = driver.find_element_by_id("u_0_3").click()
     return driver
@@ -20,7 +31,6 @@ def get_my_books_page():
     pass
 
 if __name__ == '__main__':
-    LINK                    = 'https://www.goodreads.com/'
     options                 = webdriver.ChromeOptions()
     options.binary_location = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
     options.add_argument('--headless')
@@ -31,5 +41,5 @@ if __name__ == '__main__':
     driver = gr_login(driver)
     soup                    = BeautifulSoup(driver.page_source, 'lxml')
     title                   = soup.find_all('title')[0].text
-    print(title)
+    print('Title: '+str(title))
     driver.quit()
