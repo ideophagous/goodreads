f6ce9e596af95e5b2e0afa595e16c80f8028da64
Mounir Afifi <mounir.afifi@intern.promatis.de>
2019-05-02 15:23:51 +0200

    removing commented debugging code

--- get_gr_page.py
+++ get_gr_page.py
@@ -19,12 +19,9 @@ def load_access():
 def gr_login(driver):
     EMAIL,PASSWORD = load_access()
     username       = driver.find_element_by_id("userSignInFormEmail").send_keys(EMAIL)
-    #print(username)
     password       = driver.find_element_by_id("user_password")
     password.send_keys(PASSWORD)
-    #print(password)
     password.submit()
-    #ff = driver.find_element_by_id("u_0_3").click()
     return driver
 
 def get_my_books_page():
@@ -37,7 +34,6 @@ if __name__ == '__main__':
     driver                  = webdriver.Chrome(chrome_options=options)
     driver.get(LINK)
     soup                    = BeautifulSoup(driver.page_source, 'lxml')
-    #print(soup.prettify())
     driver = gr_login(driver)
     soup                    = BeautifulSoup(driver.page_source, 'lxml')
     title                   = soup.find_all('title')[0].text
